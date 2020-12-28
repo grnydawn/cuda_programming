9f1531471625f7c300cb0782a3b1b69f3b5064f3
Youngsung Kim <youngsun@ucar.edu>
2020-12-28 16:05:22 -0500

    added bin directory

---
+++ bin/isummit.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "ERROR: charging account is missing"
+else
+	bsub -Is -W 2:00 -nnodes 1 -P $1 $SHELL
+fi
+
+
+#$ hostname
+#batch2
+#
+#$ jsrun -n1 hostname
+#a35n03
+
+module load pgi
+module load cuda
+module load nsight-systems
+module load nsight-compute
